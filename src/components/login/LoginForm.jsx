6201997259c3d54021a5d16b55b2f4a4f2bd276a
franky19 <frankimarbun@gmail.com>
2020-12-20 01:31:05 +0700

    merge branch staging "ft-ui-login" into main

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,7 +1,7 @@
 import {useState} from 'react'
 import {useDispatch} from 'react-redux'
 import { loginUserAction } from '../../redux/actions/auth.action'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 const LoginForm = () => {
     const dispatch = useDispatch()
     
