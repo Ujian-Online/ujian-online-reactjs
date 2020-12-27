40c9326b089be3484164dfd04502f0e1bd758c0e
agung <agungjati94>
2020-12-27 10:41:12 +0700

    add logo

--- src/components/login/LoginPage.jsx
+++ src/components/login/LoginPage.jsx
@@ -1,17 +1,25 @@
 import { createUseStyles } from 'react-jss'
 import LoginForm from './LoginForm'
+import { Link } from 'react-router-dom'
 
 const useStyles = createUseStyles({
-    container : {
-        height : '100vh'
-    },    
+    container: {
+        height: '100vh'
+    },
+    logo : {
+        top : '40px',
+        left : '50px'
+    }
 })
 
 const LoginPage = () => {
     const classes = useStyles()
     return (<div className={`container-fluid ${classes.container} `}  >
-        <div className='row h-100 d-flex align-items-center justify-content-center'>
-            <div className='col-md-4'>
+        <div className='row h-100 d-flex align-items-center justify-content-center position-relative'>
+            <Link to="/" className={`position-absolute d-none d-sm-block ${ classes.logo }`} >
+                <img src='/assets/img/bg-logo.png' alt='logo' width='50px' />
+            </Link>
+            <div className='col-sm-6 col-md-4'>
                 <LoginForm />
             </div>
         </div>
