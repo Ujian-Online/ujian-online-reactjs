6e07b9fe9a7ba4755e5b7d3cfe5039adc3f7061e
franky19 <frankimarbun@gmail.com>
2021-02-08 14:57:58 +0700

    CSS: Member - Daftar Ujian

--- src/components/member/daftarUjian/DaftarUjianForm.jsx
+++ src/components/member/daftarUjian/DaftarUjianForm.jsx
@@ -5,6 +5,7 @@ import { useHistory } from 'react-router-dom';
 import 'react-datepicker/dist/react-datepicker.css'
 
 
+
 const useStyles=createUseStyles({
     Pendaftar:{
         color:'gray'
@@ -18,138 +19,172 @@ const useStyles=createUseStyles({
 })
 const DaftarUjianForm=()=>{
     const classes=useStyles()
-    const [startDate, setStartDate] = useState(new Date())
+
+    //deklarasi tanggal sertifikat
+    const [stateForm, setStateForm] = useState({
+        dateSertifikat: new Date(),
+    });
+
+    //Jika sertifikat diceklis true maka akan mengisi data sertifikat lama,  jika false maka akan lanjut
     const [checked,setChecked]=useState(true);
+
+    //deklarasi untuk pindah page
     const history=useHistory()
 
-   
-const onChecked=()=>{
-       return(<div>
-<div className="form-group row">
-    <label htmlForm="nomorSertifikasi" className="col-sm-2 col-form-label"><h6>Nomor Sertifikasi</h6></label>
-    <div className="col-sm-10">
-        <input type="text" className="form-control" id="nomorSertifikasi" placeholder="Nomor Sertifikasi" required/>
-    </div>
-</div>
-<div className="form-group row">
-    <label htmlForm="uploadSertifikatLama" className="col-sm-2 col-form-label"><h6>Upload Sertifikat lama</h6></label>
-    <div className="col-sm-10">
-        <div className="col-sm-5">
-            <input type="file" className="form-control-file" id="uploadSertifikatLama"/>
-        </div>
-        <div className="col-sm-5 mt-3">
-            <button type="button" className="btn btn-primary">Upload</button>
-        </div>
-    </div>
-</div>
-</div>)
-   }
-
-    return(
-        <div>
-<div className="form was-validated">
-            <div className='text-center'>
-                <h4>Detail Informasi</h4>
-            </div>
-            <div className="form-group row">
-                <label htmlForm="Pendaftar" className="col-sm-2 col-form-label">Pendaftar</label>
-                <div className="col-sm-10">
-                <input type="text" readonly className="form-control" id="Pendaftar" value="Akbar Bintang Wicaksono" required/>
+    return (
+        <div className='container my-4' >
+            <div className='row'>
+                <div className='col text-center' >
+                    <h6>
+                        Detail Informasi
+                    </h6>
                 </div>
             </div>
-            <div className="form-group row">
-                <label htmlForm="inputSertifikasi" className="col-sm-2 col-form-label">sertifikasi</label>
-                <div className="col-sm-10">
-                    <input type="text" className="form-control" id="inputSertifikasi" placeholder="Sertifikasi" required/>
-                </div>
-            </div>
-            <div className="form-group row">
-                <label htmlForm="PilihTuk" className="col-sm-2 col-form-label">Lokasi Uji Kompetensi</label>
-                <div className="col-sm-10">
-                    <div className="form-group">
-                        <select className="custom-select" required>
-                        <option value="">Pilih Lokasi TUK</option>
-                        <option value="1">LSP Mercubuana</option>
-                        <option value="2">LSP AMIKOM Jogja</option>
-                        <option value="3">LSP Mikroskil</option>
-                        </select>
-                        <div className="invalid-feedback">Lokasi TUK tidak boleh Kosong</div>
-                    </div>
-                </div>
-            </div>
-            <div className="form-group row">
-                <label htmlForm="PilihTraining" className="col-sm-2 col-form-label">Pemilihan Training</label>
-                <div className="col-sm-10">
-                    <div className="form-check form-check-inline">
-                        <div className="custom-control custom-radio mb-3">
-                            <input type="radio" className="custom-control-input" id="Training" name="radio-stacked"required/>
-                            <label className={`custom-control-label ${classes.customLabel}`} htmlFor="Training"><h6 className={`${classes.customLabel}`}>Training</h6></label>
-                            <div className='invalid-feedback'>Apakah anda ingin mengikuti training ?</div>
-                            <div className={`valid-feedback ${classes.Training}`}>(Pemilihan training akan ada tambahan biaya training)</div>
-                        </div>
-                    </div>
-                    <div className="form-check form-check-inline">
-                        <div className="custom-control custom-radio mb-3">
-                            <input type="radio" className="custom-control-input" id="PilihTidak" name="radio-stacked" required/>
-                            <label className={`custom-control-label ${classes.customLabel}`} htmlFor="PilihTidak"><h6 className={`${classes.customLabel}`}>Tidak</h6></label>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="form-group row">
-                    <label htmlForm="SertifikasiUlang" className="col-sm-2 col-form-label">
-                        Sertifikasi Ulang
+            <div className='row mt-4 ' >
+                <div className='col-3 d-flex align-items-center' >
+                    <label className=' mb-0 ' >
+                        Pendaftar
                     </label>
-                    <div className="col-sm-10">
-                        <div className="custom-control custom-checkbox mb-3">
-                            <input type="checkbox" className="custom-control-input" id="SertifikasiUlang" checked={checked}
-                            onChange={()=>setChecked(!checked)}
-                            />
-                            <label className="custom-control-label" htmlFor="SertifikasiUlang"><h6 className={`${classes.customLabel}`}>Ceklis jika perpanjangan sertifikasi sebelumnya</h6></label>
-                            <div className="invalid-feedback">Apakah Anda Ingin memperpanjang Sertifikasi sebelumnya ?</div>
-                            <div className={`valid-feedback ${classes.Training}`}>Silahkan upload Jobdesk, surat permohonan perpanjangan, surat pernyataan bahwa ybs masih bekerja dibidang SDM oleh atasan, dan upload sertifikat lama</div>
+                </div>
+                <div className='col-9' >
+                <input type="text" readonly className="form-control" id="Pendaftar" value="Akbar Bintang Wicaksono"/>
+                </div>
+            </div>
+            <div className='row mt-4 ' >
+                <div className='col-3 d-flex align-items-center' >
+                    <label className=' mb-0 ' >
+                        Sertifikasi
+                    </label>
+                </div>
+                <div className='col-9' >
+                    <input className='form-control bg-white' placeholder='Masukan nomor sertifikasi' />
+                </div>
+            </div>
+            <div className='row mt-4 ' >
+                <div className='col-3 d-flex align-items-center' >
+                    <label className=' mb-0 ' >
+                        Lokasi Uji Kompetensi
+                    </label>
+                </div>
+                <div className='col-9' >
+                    <select className='form-control bg-white  ' >
+                        <option>Pilih Lokasi TUK</option>
+                        <option>LSP Mercubuana</option>
+                        <option>LSP Amikom Jogja</option>
+                        <option>LSP Mikroskil</option>
+                    </select>
+                </div>
+            </div>
+           
+            <div className='row mt-4 ' >
+                <div className='col-3 d-flex align-items-center' >
+                    <label className=' mb-0 ' >
+                        Pemilihan Training
+                    </label>
+                </div>
+                <div className='col-9 d-flex flex-wrap ' >
+                    <div className="form-check mr-2" >
+                        <input className="form-check-input" type="radio" name="training" id="training" defaultChecked />
+                        <label className="form-check-label" htmlFor="training">
+                            Training
+                        </label>
+                        <div className={`${classes.Training}`}>
+                            <label>
+                            (Pemilihan training akan ada tambahan biaya training)
+                            </label>
                         </div>
                     </div>
-            </div>
-            <div className="form-group row">
-                <label htmlForm="nomorSertifikasi" className="col-sm-2 col-form-label">Nomor Sertifikasi</label>
-                <div className="col-sm-10">
-                    <input type="text" className="form-control" id="nomorSertifikasi" placeholder="Nomor Sertifikasi" required/>
+                    
+                    <div className="form-check">
+                        <input className="form-check-input" type="radio" name="training" id="tidak_training" />
+                        <label className="form-check-label" htmlFor="tidak_training">
+                            Tidak
+                        </label>
+                    </div>
                 </div>
             </div>
-            <div className="form-group row">
-                <label htmlForm="inputSertifikasi" className="col-sm-2 col-form-label">Tanggal sertifikasi</label>
-                <div className="col-sm-10">
-                    <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+            <div className='row mt-4 ' >
+                 <div className='col-3 d-flex align-items-center' >
+                    <label className=' mb-0 ' >
+                        Sertifikasi
+                    </label>
                 </div>
-            </div>
-            <div className="form-group row">
-                <label htmlForm="uploadSertifikatLama" className="col-sm-2 col-form-label"><h6>Upload Sertifikat lama</h6></label>
-                    <div className="col-sm-10">
-                    <div className="input-group">
-                        <div className="custom-file">
+                <div className='col-9 d-flex flex-wrap ' >
+                    <div className="form-check mr-2">
+                        <input className="form-check-input"
+                            type="checkbox" name="sertifikasi_ulang" id="lama" value="lama" 
+                            defaultChecked
+                            onChange={()=>setChecked(checked===false)} />
+                        <label className="form-check-label" htmlFor="lama">
+                            Ceklis jika perpanjangan sertifikasi sebelumnya
+                        </label>
+                        <div className={`${classes.Training}`}>
+                            <label>
+                            (Silahkan upload Jobdesk, surat permohonan perpanjangan, surat pernyataan bahwa ybs masih bekerja dibidang SDM oleh atasan, dan upload sertifikat lama)
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+                {checked=== true ? <>
+
+                <div className='row mt-4 ' >
+                    <div className='col-3 d-flex align-items-center' >
+                        <label className=' mb-0 ' >
+                           Nomor Sertifikasi sebelumnya
+                        </label>
+                    </div>
+                    <div className='col-9' >
+                        <input className='form-control bg-white  ' placeholder="masukan nomor sertifikasi lama"/>
+                    </div>
+                </div>
+
+                <div className='row mt-4 ' >
+                    <div className='col-3 d-flex align-items-center' >
+                        <label className=' mb-0 ' >
+                            Tanggal Sertifikasi sebelumnya
+                        </label>
+                    </div>
+                    <div className='col-9 d-flex flex-wrap align-items-center ' >
+                        <DatePicker
+                            className='form-control bg-white  '
+                            selected={stateForm.dateSertifikat}
+                            onChange={dateSertifikat => setStateForm({ ...stateForm, dateSertifikat })}
+                            />
+                    </div>
+                </div>
+
+                <div className='row mt-4 ' >
+                    <div className='col-3 d-flex ' >
+                        <label className=' mb-0 ' >
+                            Upload Sertifikat lama
+                        </label>
+                    </div>
+                    <div className='col-9 input-group' >
+                         <div className="custom-file">
                             <input type="file" className="form-control-file" id="uploadSertifikatLama"/>
                         </div>
                         <div className="input-group-append">
                             <button className="btn btn-primary" type="button" id="uploadSertifikatLama">Upload</button>
                         </div>
                     </div>
-                    </div>
-            </div>
-            
-        </div>
-            <div className="col-md-12 mt-5 mb-5">
-                <div className="form-group col-sm-4 col-md-4 ml-auto mr-auto">
-                    <button type="submit" className="btn btn-primary btn-block">
-                        Pesan Sekarang
-                    </button>
                 </div>
-                <div className="form-group col-sm-6 col-md-4 mr-auto ml-auto">
-                    <button type="submit" className="btn btn-light bg-gray btn-block" onClick={() => history.push('/')}>
-                            Kembali
-                    </button>
-                </div>      
-            </div>
+
+                </> : ''}
+                <div className='row mt-4 ' >
+                    <div className="col-md-12 mt-5 mb-5">
+                        <div className="form-group col-sm-4 col-md-4 ml-auto mr-auto">
+                            <button type="submit" className="btn btn-primary btn-block">
+                                Pesan Sekarang
+                            </button>
+                        </div>
+                        <div className="form-group col-sm-6 col-md-4 mr-auto ml-auto">
+                            <button type="submit" className="btn btn-light bg-gray btn-block" onClick={() => history.push('/member/ujian-baru')}>
+                                    Batal
+                            </button>
+                        </div>      
+                </div>
+            </div> 
         </div>
     )
 }
