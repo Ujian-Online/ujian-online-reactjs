f17e91eb0417d2f3181a58853143cf8eccbaacd8
franky19 <frankimarbun@gmail.com>
2021-02-17 16:11:47 +0700

    Member: Implement Halaman Menunggu Pembayaran

--- src/components/member/MenungguPembayaran/MenungguPembayaran.jsx
+++ src/components/member/MenungguPembayaran/MenungguPembayaran.jsx
@@ -1,10 +1,11 @@
+import { Modal } from 'react-bootstrap'
 import { useEffect } from 'react'
 import {useState} from 'react'
 import DataTable from 'react-data-table-component'
 import { useDispatch, useSelector } from 'react-redux'
-import { useHistory } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { getOrderAction } from '../../../redux/actions/order.action'
-import {status_bayar,Aksi, Lihat} from './status'
+import {status_bayar} from './status'
 
 const customStyles = {
     headCells: {
@@ -53,41 +54,54 @@ const MenungguPembayaran=()=>{
     useEffect(()=>{
         console.log('order list',order)
         setSkema([ ...(order.order || [] ).map(o=>({
-            no:o.no,
-            skema_sertifikasi:o.skema_sertifikasi,
+            no:o.sertifikasi_id,
+            skema_sertifikasi:o.sertifikasi.title,
             status_bayar:o.status,
         }))])
     },[order.order])
-
-    // //button Aksi tampil berdasarkan badges
-    // const [stateForm, setStateForm] = useState({
-    //    Menunggu_Pembayaran:status_bayar,
-    // });
-
-
-
-
-    // const [skema,setSkema]=useState([
-    //     {
-    //         no: 1,
-    //         skema_sertifikasi:'SuperVisor Sumber daya manusia',
-    //         status_bayar:'Menunggu Pembayaran',
-    //         Aksi:stateForm.Menunggu_Pembayaran||status_bayar==='Menunggu Pembayaran'?'Upload Pembayaran':''
-    //     },
-    //     {
-    //         no: 2,
-    //         skema_sertifikasi:'Manager Pengelolaan Sumber Daya Manusia',
-    //         status_bayar:'Terbayar',
-    //         Aksi:stateForm.Menunggu_Pembayaran||status_bayar==='Terbayar'?'Lihat':''
-    //     },
-    //     {
-    //         no: 3,
-    //         skema_sertifikasi:'Talent Manager',
-    //         status_bayar:'Menunggu Verifikasi',
-    //         Aksi:stateForm.Menunggu_Pembayaran||status_bayar==='Menunggu Verifikasi'?'Upload Ulang':'',
-    //         Lihat:stateForm.Menunggu_Pembayaran||status_bayar==='Menunggu Verifikasi'?'Lihat':''
-    //     }
-    // ])
+    
+    //state untuk tampil data dalam Modal
+    const [stateForm, setStateForm] = useState({
+        email:'',
+        transfer_from_bank_name:'',
+        status:'',
+        transfer_to_bank_name:'',
+    });
+     //Modal untuk Lihat data Pesanan
+     const [showModal, setShowModal] = useState(false);
+     const handleCloseModal = () => setShowModal(false)
+     const handleShowModal = () => setShowModal(true);
+    
+     const renderModalLihatPesanan = () => (
+         <Modal show={showModal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Bukti Pembayaran</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className="form-group">
+                        <label className="col-md-3 col-sm-3">Asal bank</label>
+                        <label className="col-md-2 col-sm-2">:</label>
+                         <label htmlFor='AsalBank'>{()=>setStateForm(...stateForm , order.transfer_from_bank_name)}</label>
+                        {/* <label htmlFor='AsalBank'>MANDIRI</label> */}
+                    </div>
+                    <div className="form-group">
+                        <label className="col-md-3 col-sm-3">Bank tujuan</label>
+                        <label className="col-md-2 col-sm-2">:</label>
+                        <label htmlFor='bankTujuan'>{()=>setStateForm(...stateForm , order.transfer_to_bank_name)}</label>
+                    </div>
+                    <div className="form-group">
+                        <label className="col-md-3 col-sm-3">Nama Pengirim</label>
+                        <label className="col-md-2 col-sm-2">:</label>
+                        <label htmlFor='NamaPengirim'>{()=>setStateForm(...stateForm , order.user.email)}</label>
+                    </div>
+                    <div className="form-group">
+                        <label className="col-md-3 col-sm-3">Status</label>
+                        <label className="col-md-2 col-sm-2">:</label>
+                        <label htmlFor='Status'>{()=>setStateForm(...stateForm , order.status)}</label>
+                    </div>
+                </Modal.Body>
+         </Modal>
+     )
 
     const columns=[
         {selector:'no',name:'no', sortable:true},
@@ -96,12 +110,52 @@ const MenungguPembayaran=()=>{
             format:row => status_bayar[row.status_bayar]
         },
         {selector:'Aksi',name:'Aksi',sortable:true,
-            format: row => Aksi[row.Aksi]},{selector:'Aksi',
-        format: row => Lihat[row.Lihat]
+            format:row=>{
+                if(row.status_bayar==='waiting_payment'){      
+                    return <button className='btn btn-success' style={{ padding: '2px 10px' , fontSize : '14px' }}>
+                    <Link to ="/member/order/:id" className="text-light">Upload Pembayaran</Link> 
+                      </button>
+                }
+                else if(row.status_bayar==='payment_rejected'){
+                    return <button className='btn btn-warning' style={{ padding: '2px 10px' , fontSize : '14px' }}>
+                    <Link to ="/member/order/:id" className="text-light">Upload Ulang</Link> 
+                      </button>
+                }
+                else if(row.status_bayar==='pending_verification'){
+                    return 
+                    <div className='row' >
+                                <div className='col-7 d-flex align-items-center' >
+                                    <button className='btn btn-warning' style={{ padding: '2px 10px' , fontSize : '14px' }}>
+                                        <Link to ="/member/order/:id" className="text-light">Upload Ulang</Link> 
+                                    </button>
+                                </div>
+                                <div className='col-5' >
+                                    <button className='btn btn-primary' style={{ padding: '2px 10px' , fontSize : '14px' }} onClick={handleShowModal}>
+                                            Lihat
+                                    </button>
+                                </div>
+                            </div>
+                   
+                     
+                      
+                }
+                else if(row.status_bayar==='payment_verified'){
+                    return <button className='btn btn-success' style={{ padding: '2px 10px' , fontSize : '14px' }}>
+                            Lihat
+                      </button>
+                }
+                else{
+                    return 'status bayar tidak terdeteksi'
+                }
+                
+            }
+        //     format: row => Aksi[row.Aksi]},{selector:'Aksi',
+        // format: row => Lihat[row.Lihat]
     },
     ];
 
     return(
+        <>
         <div className='container mt-4'>
             <div className="card">
                 <h5 className="card-header text-center bg-white">List Pembayaran</h5>
@@ -110,13 +164,14 @@ const MenungguPembayaran=()=>{
                         columns={columns}
                         data={skema}
                         customStyles={customStyles}
-                        wrap={Aksi}
                         noHeader
                     />
-                    
                 </div>
             </div>
-        </div>)
+        </div>
+        {renderModalLihatPesanan()}
+        </>
+        )
 }
 
 export default MenungguPembayaran
