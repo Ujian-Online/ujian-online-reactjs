e2c8832643ad9837f2926a0cbe4525129588a6b4
franky19 <frankimarbun@gmail.com>
2021-02-18 15:44:37 +0700

    Member: Implement Halaman Menunggu Pembayaran

--- src/components/member/MenungguPembayaran/MenungguPembayaran.jsx
+++ src/components/member/MenungguPembayaran/MenungguPembayaran.jsx
@@ -3,8 +3,8 @@ import { useEffect } from 'react'
 import {useState} from 'react'
 import DataTable from 'react-data-table-component'
 import { useDispatch, useSelector } from 'react-redux'
-import { Link, useHistory, useParams } from 'react-router-dom'
-import { getOrderAction,getDetailOrderAction } from '../../../redux/actions/order.action'
+import { Link, useHistory, useParams, useRouteMatch } from 'react-router-dom'
+import { getOrderAction,getDetailOrderAction, sendProofPaymentAction } from '../../../redux/actions/order.action'
 import {status_bayar} from './status'
 
 const customStyles = {
@@ -34,11 +34,8 @@ const customStyles = {
 
 }
 
-const MenungguPembayaran=()=>{
+const MenungguPembayaran=(props)=>{
     const history = useHistory()
-    
-    //parameter ID
-    const{orderId} = useParams()
 
     // useEffect(() => {
     //     dispatch(getDetailOrderAction(auth.token, id))
@@ -51,7 +48,7 @@ const MenungguPembayaran=()=>{
     const dispatch=useDispatch()
     useEffect(()=>{
         dispatch(getOrderAction(auth.token))
-    },[auth.token])
+    },[])
 
     //deklarasi reducer order
     const order=useSelector(state=>state.order)
@@ -92,31 +89,77 @@ const MenungguPembayaran=()=>{
                     <div className="form-group">
                         <label className="col-md-3 col-sm-3">Asal bank</label>
                         <label className="col-md-2 col-sm-2">:</label>
-                         {/* <label htmlFor='AsalBank'>{()=>setStateForm(...stateForm , order.transfer_from_bank_name)}</label> */}
-                        <label htmlFor='AsalBank'>{order.transfer_from_bank_name}</label>
+                        <span className='ml-2' >{()=>setStateForm({...stateForm,...stateForm.transfer_from_bank_name})}</span>
                     </div>
                     <div className="form-group">
                         <label className="col-md-3 col-sm-3">Bank tujuan</label>
                         <label className="col-md-2 col-sm-2">:</label>
-                        <label htmlFor='AsalBank'>{order.transfer_to_bank_name}</label>
-                        {/* <label htmlFor='bankTujuan'>{()=>setStateForm(...stateForm , order.transfer_to_bank_name)}</label> */}
+                        <span className='ml-2' >{detailOrder.transfer_to_bank_name}</span>
+
                     </div>
                     <div className="form-group">
                         <label className="col-md-3 col-sm-3">Nama Pengirim</label>
                         <label className="col-md-2 col-sm-2">:</label>
-                        <label htmlFor='AsalBank'>{user.email}</label>
+                        <span className='ml-2' >{user.email}</span>
                         {/* <label htmlFor='NamaPengirim'>{()=>setStateForm(...stateForm , order.user.email)}</label> */}
                     </div>
                     <div className="form-group">
                         <label className="col-md-3 col-sm-3">Status</label>
                         <label className="col-md-2 col-sm-2">:</label>
-                        <label htmlFor='AsalBank'>{order.status}</label>
+                        <span className='ml-2' >{detailOrder.status}</span>
                         {/* <label htmlFor='Status'>{()=>setStateForm(...stateForm , order.status)}</label> */}
                     </div>
                 </Modal.Body>
          </Modal>
      )
+     const {id} =  props.match.params
+     useEffect(()=>{
+        dispatch(getDetailOrderAction(id))
+    },[])
 
+    const clickDetail = () => {
+        history.push('/member/order/'+id)
+    }
+    //  useEffect(() => {
+    //     dispatch(getDetailOrderAction(id))
+    // }, [])
+
+     const [stateForm,setStateForm]=useState({
+        id:order.id,
+        transfer_from_bank_name:order.transfer_from_bank_name,
+        transfer_to_bank_name:order.transfer_to_bank_name,
+        email:user.email,
+        status:order.status
+     })
+     useEffect(() => {
+            setStateForm({
+                ...stateForm,
+                transfer_from_bank_name:order.transfer_from_bank_name,
+                transfer_to_bank_name:order.transfer_to_bank_name,
+                email:user.email,
+                status:order.status
+            })
+        }, [order ] )
+//    const [stateForm, setStateForm] = useState({
+//         sertifikasi_id: uploadBukti.id
+        
+//     });
+    // // const [stateForm, setStateForm] = useState({
+    // //    transfer_from_bank_name:id.transfer_from_bank_name,
+    // //    transfer_to_bank_name:id.transfer_to_bank_name,
+    // //    status:id.status
+    // // });
+
+    // useEffect(() => {
+    //     setStateForm([ ...stateForm(order || [] ).map(o=>({
+    //         transfer_from_bank_name:o.transfer_from_bank_name,
+    //        transfer_to_bank_name:o.transfer_to_bank_namer,
+    //        status_bayar:o.status,
+    //        email:o.user.email
+    //     }))])
+    // }, [order ] )
+
+    //columns data
     const columns=[
         {selector:'no',name:'no', sortable:true},
         {selector:'skema_sertifikasi',name:'Skema Sertifikasi',sortable:true},
@@ -125,24 +168,24 @@ const MenungguPembayaran=()=>{
         },
         {selector:'Aksi',name:'Aksi',sortable:true,
             format:row=>{
+                //view button berdasarkan badges
                 if(row.status_bayar==='waiting_payment'){      
                     return <button className='btn btn-success' style={{ padding: '2px 10px' , fontSize : '14px' }}
-                    onClick={() => history.push('/member/order/:id')}>
+                    onClick={clickDetail}>
                         Upload Pembayaran
                       </button>
                 }
                 else if(row.status_bayar==='payment_rejected'){
                     return <button className='btn btn-warning' style={{ padding: '2px 10px' , fontSize : '14px' }}
-                    onClick={() => history.push('/member/order/:id')}>
+                    onClick={clickDetail}>
                         Upload Ulang
                       </button>
                 }
                 else if(row.status_bayar==='pending_verification'){
-                    return 
-                    <div className='row' >
+                    return <div className='row' >
                                 <div className='col-7 d-flex align-items-center' >
                                     <button className='btn btn-warning' style={{ padding: '2px 10px' , fontSize : '14px' }}
-                                    onClick={() => history.push('/member/order/:id')}>
+                                    onClick={clickDetail}>
                                         Upload Ulang
                                     </button>
                                 </div>
@@ -152,9 +195,6 @@ const MenungguPembayaran=()=>{
                                     </button>
                                 </div>
                             </div>
-                   
-                     
-                      
                 }
                 else if(row.status_bayar==='payment_verified'){
                     return <button className='btn btn-success' style={{ padding: '2px 10px' , fontSize : '14px' }} onClick={handleShowModal}>
