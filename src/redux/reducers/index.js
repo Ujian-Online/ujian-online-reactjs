6ad11212bd08d98f26cc57b8f6f3a7e13332e654
Dipras <dipras75@gmail.com>
2021-03-01 10:45:38 +0700

    halaman ujian

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import authReducer from './auth.reducer'
 import sertifikasiReducer from './sertifikasi.reducer'
 import orderReducer from './order.reducer'
 import apl01Reducer from './apl01.reducer'
+import ujianReducer from './ujian.reducer'
 import { combineReducers } from 'redux'
 
 export const rootReducer = combineReducers({
     auth : authReducer,
     sertifikasi:sertifikasiReducer,
     order:orderReducer,
-    apl01: apl01Reducer
+    apl01: apl01Reducer,
+    ujian: ujianReducer
 });
---
+++ src/redux/types/ujian.type.js
@@ -0,0 +1,5 @@
+export const SET_ANSWER = 'SET_ANSWER';
+export const CLEAR_ANWSER = 'CLEAR_ANWSER';
+export const DELETE_ANSWER = 'DELETE_ANSWER';
+export const SET_SOAL_ID = 'SET_SOAL_ID';
+export const CLEAR_SOAL_ID = 'CLEAR_SOAL_ID';
